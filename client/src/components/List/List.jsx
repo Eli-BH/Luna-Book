ba4fcf0b77ea7744dba2a380f8c6565accc3eb9c
Eli-BH <eli.i.henderson95@gmail.com>
2021-08-09 18:18:34 -0400

    [client] map working

--- client/src/components/List/List.jsx
+++ client/src/components/List/List.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import {CircularProgress, Grid, Typography, InputLabel, MenuItem, FormControl} from '@material-ui/core';
+import PlaceDetails from '../PlaceDetails/PlaceDetails';
 
+import useStyles from './styles';
 const List = () => {
-    return <div>List</div>;
+    const classes = useStyles();
+
+    const places = [{name: 'Cool Place'}, {name: 'comic stop'}, {name: 'Best comics'}, {name: 'Cool Place'}, {name: 'comic stop'}, {name: 'Best comics'}, {name: 'Cool Place'}, {name: 'comic stop'}, {name: 'Best comics'}];
+
+    return (
+        <div className={classes.container}>
+            <Typography variant="h4">Comic Shops around you!</Typography>
+            <Grid container spacing={3} className={classes.list}>
+                {places?.map((place, i) => (
+                    <Grid item key={i} xs={12}>
+                        <PlaceDetails place={place} />
+                    </Grid>
+                ))}
+            </Grid>
+        </div>
+    );
 };
 
 export default List;
